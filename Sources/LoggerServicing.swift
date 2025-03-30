7e7c7d01128b7aa8b3f5bb4ea1d1f998db5ab327
Petter Brakalsvalet <Petter.Braka@gmail.com>
2025-03-30 19:35:44 +0100

    Updated LoggerService to be Swift 6 compliant

--- Sources/LoggerServicing.swift
+++ Sources/LoggerServicing.swift
@@ -5,17 +5,17 @@
 //  Created by Petter vang Brakalsvålet on 16/10/2024.
 //
 
-public protocol LoggerServicing: Sendable {
+public protocol LoggerServicing {
     
     /// Enable logging for specific categories
-    nonisolated func enable(_ categories: LogCategory...) async
+    func enable(_ categories: LogCategory...) async
     
     /// Disable logging for specific categories
-    nonisolated func disable(_ categories: LogCategory...) async
+    func disable(_ categories: LogCategory...) async
     
     /// Set logging level
-    nonisolated func set(levels: LogLevel...) async
+    func set(levels: LogLevel...) async
     
     /// Sends a message to the console
-    nonisolated func log(category: LogCategory, message: String, error: Error?, level: LogLevel) async
+    func log(category: LogCategory, message: String, error: Error?, level: LogLevel) async
 }
