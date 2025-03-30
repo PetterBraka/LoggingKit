70ce957a089c5d6e37cdcd7371fa66df3f51bb3b
Petter Brakalsvalet <Petter.Braka@gmail.com>
2025-03-30 19:42:23 +0100

    Removed async from protocol

--- Sources/LoggerServicing.swift
+++ Sources/LoggerServicing.swift
@@ -8,14 +8,14 @@
 public protocol LoggerServicing {
     
     /// Enable logging for specific categories
-    func enable(_ categories: LogCategory...) async
+    func enable(_ categories: LogCategory...)
     
     /// Disable logging for specific categories
-    func disable(_ categories: LogCategory...) async
+    func disable(_ categories: LogCategory...)
     
     /// Set logging level
-    func set(levels: LogLevel...) async
+    func set(levels: LogLevel...)
     
     /// Sends a message to the console
-    func log(category: LogCategory, message: String, error: Error?, level: LogLevel) async
+    func log(category: LogCategory, message: String, error: Error?, level: LogLevel)
 }
